44bb1c3af8af545367edf8e795b3f0238c63f5c6
jibjoub <jean-baptiste.despujol@epita.fr>
2024-04-08 14:15:14 +0200

    Fix the all the AlbumRepositroyImplTests

--- data/src/main/java/com/example/data/repository/AlbumsRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/AlbumsRepositoryImpl.kt
@@ -1,16 +1,31 @@
 package com.example.data.repository
 
 import com.example.common.DataState
+import com.example.data.entity.mapToAlbumModel
 import com.example.data.remote.api.AlbumApi
 import com.example.domain.model.AlbumModel
 import com.example.domain.repository.AlbumsRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class AlbumsRepositoryImpl
     @Inject
     constructor(private val albumApi: AlbumApi) : AlbumsRepository {
-        override fun getAlbums(): Flow<DataState<List<AlbumModel>>> {
-            TODO("Not yet implemented")
-        }
+        override fun getAlbums(): Flow<DataState<List<AlbumModel>>> =
+            flow { emit(albumApi.getAlbums()) }.map { response ->
+                if (response.isSuccessful.not()) {
+                    return@map DataState.Error(Exception("Response is not successful. Code: ${response.code()}"))
+                }
+                val body = response.body()
+                if (body.isNullOrEmpty()) {
+                    return@map DataState.Error(Exception("Response body is null or empty"))
+                } else {
+                    return@map DataState.Success(body.map { it.mapToAlbumModel() })
+                }
+            }.catch {
+                DataState.Error(Exception(it))
+            }
     }
